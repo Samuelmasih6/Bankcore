aec23f4d48cd8ac25519abda5b632ab0114aca62
malloced <bytewalker@localhost.localdomain>
2026-06-29 16:42:31 +0530

    add postgres service

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -16,7 +16,7 @@ var testDB *pgxpool.Pool
 func TestMain(m *testing.M) {
 	err := godotenv.Load("../../.env")
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("No .env file found")
 	}
 
 	dbSource := os.Getenv("DB_SOURCE")
