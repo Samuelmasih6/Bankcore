2576901ee075c7f3d7dba8afbccef360972af9b4
malloced <bytewalker@localhost.localdomain>
2026-07-04 20:44:38 +0530

    implement tranfer money API with custom param validator

--- api/server.go
+++ api/server.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	db "github.com/Samuelmasih6/Bankcore/db/sqlc"
+	"github.com/go-playground/validator/v10"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 // server serves all http request for our banking service
@@ -17,9 +19,14 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
+	router.POST("/accounts", server.createTransfer)
 	server.router = router
 	return server
 }
